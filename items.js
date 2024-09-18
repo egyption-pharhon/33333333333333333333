d6397828aa07e01c02f6298a521e45470edc5468
egyption-pharhon <egyptionpharhon@gmail.com>
2024-09-18 07:02:37 +0200

    Update items.js

--- items.js
+++ items.js
@@ -1,95 +1,31 @@
 // product in webpage
-fetch('product.json')
-		.then(response => response.json())
-		.then(data => {
-			const products = document.querySelector('.shop .content');
-			data.forEach( product => {
-				products.innerHTML += `
-					<div class="box">
-						<div class="image">
-							<img src="${product.img}">
-						</div>
-						<div class="stars">
-							<i class="fas fa-star"></i>
-							<i class="fas fa-star"></i>
-							<i class="fas fa-star"></i>
-							<i class="fas fa-star"></i>
-							<i class="fas fa-star"></i>
-						</div>
-						<p>${product.name}</p>
-						<span>$${product.price}</span>
-						<button onclick="openProduct()" >Add Cart</button>
-					</div>`
+function addproducts(fileName, locationOfProducts){
+	fetch('product.json')
+			.then(response => response.json())
+			.then(data => {
+				const products = document.querySelector('.shop .content');
+				data.forEach( product => {
+					products.innerHTML += `
+						<div class="box">
+							<div class="image">
+								<img src="${product.img}">
+							</div>
+							<div class="stars">
+								<i class="fas fa-star"></i>
+								<i class="fas fa-star"></i>
+								<i class="fas fa-star"></i>
+								<i class="fas fa-star"></i>
+								<i class="fas fa-star"></i>
+							</div>
+							<p onclick="openProduct()">${product.name}</p>
+							<span>$${product.price}</span>
+							<button>Add Cart</button>
+						</div>`
+				})
 			})
-		})
-fetch('featured.json')
-		.then(response => response.json())
-		.then(data => {
-			const products = document.querySelector('.featured .content');
-			data.forEach( product => {
-				products.innerHTML += `
-					<div class="box">
-						<div class="image">
-							<img src="${product.img}">
-						</div>
-						<div class="stars">
-							<i class="fas fa-star"></i>
-							<i class="fas fa-star"></i>
-							<i class="fas fa-star"></i>
-							<i class="fas fa-star"></i>
-							<i class="fas fa-star"></i>
-						</div>
-						<p>${product.name}</p>
-						<span>$${product.price}</span>
-						<button>Add Cart</button>
-					</div>`
-			})
-		})
-fetch('dresses&jumpsuits.json')
-		.then(response => response.json())
-		.then(data => {
-			const products = document.querySelector('.dresses-jumpsuits .content');
-			data.forEach( product => {
-				products.innerHTML += `
-					<div class="box">
-						<div class="image">
-							<img src="${product.img}">
-						</div>
-						<div class="stars">
-							<i class="fas fa-star"></i>
-							<i class="fas fa-star"></i>
-							<i class="fas fa-star"></i>
-							<i class="fas fa-star"></i>
-							<i class="fas fa-star"></i>
-						</div>
-						<p>${product.name}</p>
-						<span>$${product.price}</span>
-						<button>Add Cart</button>
-					</div>`
-			})
-		})
-fetch('shoes.json')
-		.then(response => response.json())
-		.then(data => {
-			const products = document.querySelector('.shoes .content');
-			data.forEach( product => {
-				products.innerHTML += `
-					<div class="box">
-						<div class="image">
-							<img src="${product.img}">
-						</div>
-						<div class="stars">
-							<i class="fas fa-star"></i>
-							<i class="fas fa-star"></i>
-							<i class="fas fa-star"></i>
-							<i class="fas fa-star"></i>
-							<i class="fas fa-star"></i>
-						</div>
-						<p>${product.name}</p>
-						<span>$${product.price}</span>
-						<button>Add Cart</button>
-					</div>`
-			})
-		})
-			
-		
+}
+addproducts('product.json', '.shop .content');
+addproducts('featured.json', '.featured .content');
+addproducts('dresses&jumpsuits.json', '.dresses-jumpsuits .content');
+addproducts('shoes.json', '.shoes .content');
+addproducts('featured.json', '.our-product .content');	
