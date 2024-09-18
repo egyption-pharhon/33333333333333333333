bf25ee41eec9febce4f9765d38128d56a0d21d78
egyption-pharhon <egyptionpharhon@gmail.com>
2024-09-18 23:45:48 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,55 +1,64 @@
 // open list items @ media 767px
-
-function openNavList(){
-	let navlist = document.querySelector('nav');
-	navlist.classList.toggle("show");
+function openNavList() {
+    let navlist = document.querySelector('nav');
+    navlist.classList.toggle("show");
 }
 
 // open details of product
-let allproduct =[]
+let allproduct = [];
 
-
-function openProduct(id){
-	productDelails(id)
-
-	window.open("product.html", "_self")
-}
-//  change color of product
-let activeImage = document.querySelector('.product .images .active')
-
-function changeColor(newImage){
-	activeImage.src = newImage
-}
-	const activeOne = document.querySelector('.product .images .color-product #one')
-	const activeTwo = document.querySelector('.product .images .color-product #two')
-const activeThree = document.querySelector('.product .images .color-product #three')
-	const activeFour = document.querySelector('.product .images .color-product #four')
-	const activeDetails = document.querySelector('.product .images .info p')
-function productDelails(id){
-	const activeProduct = allproduct[id]
-
-	activeImage.src = activeProduct.img
-
-	activeOne.src = activeProduct.productColors[0]
-
-	activeTwo.src = activeProduct.productColors[1]
-	
-	activeThree.src = activeProduct.productColors[2]
-
-	activeFour.src = activeProduct.productColors[3]
-
-	activeDetails.src = activeProduct.ProductDetails
+function openProduct(id) {
+    productDetails(id);
+    window.open("product.html", "_self");
 }
 
+// change color of product
+let activeImage = document.querySelector('.product .images .active');
 
+function changeColor(newImage) {
+    if (activeImage) {
+        activeImage.src = newImage;
+    } else {
+        console.error('Active image element not found');
+    }
+}
 
+const activeOne = document.querySelector('.product .images .color-product #one');
+const activeTwo = document.querySelector('.product .images .color-product #two');
+const activeThree = document.querySelector('.product .images .color-product #three');
+const activeFour = document.querySelector('.product .images .color-product #four');
+const activeDetails = document.querySelector('.product .images .info p');
 
+function productDetails(id) {
+    const activeProduct = allproduct[id];
 
+    if (activeProduct) {
+        if (activeImage) {
+            activeImage.src = activeProduct.img;
+        }
 
+        if (activeOne) {
+            activeOne.src = activeProduct.productColors[0];
+        }
 
+        if (activeTwo) {
+            activeTwo.src = activeProduct.productColors[1];
+        }
 
+        if (activeThree) {
+            activeThree.src = activeProduct.productColors[2];
+        }
 
+        if (activeFour) {
+            activeFour.src = activeProduct.productColors[3];
+        }
 
-
-
-
+        if (activeDetails) {
+            activeDetails.textContent = activeProduct.ProductDetails; // استخدام textContent بدلاً من src
+        } else {
+            console.error('Active details element not found');
+        }
+    } else {
+        console.error('Active product not found for ID:', id);
+    }
+}
