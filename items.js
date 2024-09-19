f8caf2143ed5c92849ccc6546f730b936018fc8a
egyption-pharhon <egyptionpharhon@gmail.com>
2024-09-19 06:29:34 +0200

    Update items.js

--- items.js
+++ items.js
@@ -1,103 +1,3 @@
-// open list items @ media 767px
-
-function openNavList(){
-	let navlist = document.querySelector('nav');
-	navlist.classList.toggle("show");
-}
-
-// open details of product
-let allproduct =[]
-
-let activeImage;
-let featureProduct;
-let activeProductDetails = document.querySelector('.contentOfPage')
-
-	function openProduct(id) {
-		activeProductDetails.innerHTML =`
-			 <!-- ======================start product====================== -->
-			 <div class="product">
-				 <div class="container">
-					 <div class="images">
-						 <img src="${ allproduct[id].img}" class="active">
-						 <div class="color-product">
-							 <img src="${ allproduct[id].productColors[0]}" onclick="changeColor(this.src)" id ="one">
-							 <img src="${ allproduct[id].productColors[1]}" onclick="changeColor(this.src)" id ="two">
-							 <img src="${ allproduct[id].productColors[2]}" onclick="changeColor(this.src)" id ="three">
-							 <img src="${ allproduct[id].productColors[3]}" onclick="changeColor(this.src)" id ="four">
-						 </div>
-					 </div>
-					 <div class="info">
-						 <span>${ allproduct[id].category}</span>
-						 <h3>${ allproduct[id].name}</h3>
-						 <span class="price">${allproduct[id].price}</span>
-						 <select>
-							 <option>Select Size</option>
-							 <option>XL</option>
-							 <option>XXL</option>
-							 <option>Small</option>
-							 <option>Large</option>
-						 </select>
-						 <input type="number" value="1">
-						 <button>Add to Cart</button>
-						 <h4>Product Details</h4>
-						 <p>${ allproduct[id].ProductDetails}</p>
-					 </div>
-				 </div>
-			 </div>
-			 <!-- ======================end product====================== -->
-			 <!-- ======================start other products====================== -->
-			 <div class="products our-product" id="product">
-				 <div class="container">
-					 <div class="header">
-						 <h3>Related Products</h3>
-					 </div>
-					 <div class="content">
-                    
-					 </div>
-				 </div>
-			 </div>
-			 <!-- ======================end other products====================== -->
-	 `; 
-	 activeImage = document.querySelector('.product .images .active')
-setTimeout(() => {
-	featureProduct = document.querySelector('#product .container .content')
-	fetch('product.json')
-			.then(response => response.json())
-			.then(data => {
-				featureProduct
-				allproduct =data;
-
-				
-				data.forEach( product => {
-					if(product.section === "Featured"){
-					featureProduct.innerHTML += `
-						<div class="box">
-							<div class="image">
-								<img src="${product.img}">
-							</div>
-							<div class="stars">
-								<i class="fas fa-star"></i>
-								<i class="fas fa-star"></i>
-								<i class="fas fa-star"></i>
-								<i class="fas fa-star"></i>
-								<i class="fas fa-star"></i>
-							</div>
-							<p onclick="openProduct(${product.id})" product-id="${product.id}">${product.name}</p>
-							<span>$${product.price}</span>
-							<button>Add Cart</button>
-						</div>`
-					}})
-			})
-}, 100);
-}
-//  change color of product
- 
-
-function changeColor(newImage){
-	activeImage.src = newImage
-}
-
-    console.log(featureProduct); // تحقق هنا
 // product in webpage
 function addproducts(fileName, locationOfProducts){
 	fetch('product.json')
@@ -105,8 +5,6 @@ function addproducts(fileName, locationOfProducts){
 			.then(data => {
 				const products = document.querySelector(locationOfProducts);
 				allproduct =data;
-
-				
 				data.forEach( product => {
 					if(product.section === fileName){
 					products.innerHTML += `
@@ -126,71 +24,13 @@ function addproducts(fileName, locationOfProducts){
 							<button>Add Cart</button>
 						</div>`
 				}else{
-				console.log("erro")	
-			}})
+					console.log("erro")	
+					}
+				})
 			})
 }
 addproducts('shop', '.shop .content');
 addproducts('Featured', '.featured .content');
 addproducts('Dresses', '.dresses-jumpsuits .content');
 addproducts('Shoes', '.shoes .content');
-  // <div class="box t-shirt">
-		// 				<div class="image">
-		// 					<img src="img/shop/1.jpg">
-		// 				</div>
-		// 				<div class="stars">
-		// 					<i class="fas fa-star"></i>
-		// 					<i class="fas fa-star"></i>
-		// 					<i class="fas fa-star"></i>
-		// 					<i class="fas fa-star"></i>
-		// 					<i class="fas fa-star"></i>
-		// 				</div>
-		// 				<p>Sport Boots</p>
-		// 				<span>$92.00</span>
-		// 				<button type="">Buy Now</button>
-		// 			</div>
-		// 			<div class="box">
-		// 				<div class="image">
-		// 					<img src="img/shop/2.jpg">
-		// 				</div>
-		// 				<div class="stars">
-		// 					<i class="fas fa-star"></i>
-		// 					<i class="fas fa-star"></i>
-		// 					<i class="fas fa-star"></i>
-		// 					<i class="fas fa-star"></i>
-		// 					<i class="fas fa-star"></i>
-		// 				</div>
-		// 				<p>Sport Boots</p>
-		// 				<span>$92.00</span>
-		// 				<button type="">Buy Now</button>
-		// 			</div>
-		// 			<div class="box">
-		// 				<div class="image">
-		// 					<img src="img/shop/3.jpg">
-		// 				</div>
-		// 				<div class="stars">
-		// 					<i class="fas fa-star"></i>
-		// 					<i class="fas fa-star"></i>
-		// 					<i class="fas fa-star"></i>
-		// 					<i class="fas fa-star"></i>
-		// 					<i class="fas fa-star"></i>
-		// 				</div>
-		// 				<p>Sport Boots</p>
-		// 				<span>$92.00</span>
-		// 				<button type="">Buy Now</button>
-		// 			</div>
-		// 			<div class="box">
-		// 				<div class="image">
-		// 					<img src="img/shop/4.jpg">
-		// 				</div>
-		// 				<div class="stars">
-		// 					<i class="fas fa-star"></i>
-		// 					<i class="fas fa-star"></i>
-		// 					<i class="fas fa-star"></i>
-		// 					<i class="fas fa-star"></i>
-		// 					<i class="fas fa-star"></i>
-		// 				</div>
-		// 				<p>Sport Boots</p>
-		// 				<span>$92.00</span>
-		// 				<button type="">Buy Now</button>
-		// 			</div>
+	
