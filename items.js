b5e4c979b5e45f59c0c4d27c07fe435d8feb4b5a
egyption-pharhon <egyptionpharhon@gmail.com>
2024-09-19 05:48:52 +0200

    Update items.js

--- items.js
+++ items.js
@@ -1,121 +1,98 @@
-
-// open list items @ media 767px
-
-function openNavList(){
-	let navlist = document.querySelector('nav');
-	navlist.classList.toggle("show");
+// Open navigation list at media 767px
+function openNavList() {
+    const navlist = document.querySelector('nav');
+    navlist.classList.toggle("show");
 }
 
-// open details of product
-let allproduct =[]
-
+// Global variables
+let allproduct = [];
 let activeImage;
 let featureProduct;
-let activeProductDetails = document.querySelector('.contentOfPage')
+const activeProductDetails = document.querySelector('.contentOfPage');
 
-	function openProduct(id) {
-		activeProductDetails.innerHTML =`
-			 <!-- ======================start product====================== -->
-			 <div class="product">
-				 <div class="container">
-					 <div class="images">
-						 <img src="${ allproduct[id].img}" class="active">
-						 <div class="color-product">
-							 <img src="${ allproduct[id].productColors[0]}" onclick="changeColor(this.src)" id ="one">
-							 <img src="${ allproduct[id].productColors[1]}" onclick="changeColor(this.src)" id ="two">
-							 <img src="${ allproduct[id].productColors[2]}" onclick="changeColor(this.src)" id ="three">
-							 <img src="${ allproduct[id].productColors[3]}" onclick="changeColor(this.src)" id ="four">
-						 </div>
-					 </div>
-					 <div class="info">
-						 <span>${ allproduct[id].category}</span>
-						 <h3>${ allproduct[id].name}</h3>
-						 <span class="price">${allproduct[id].price}</span>
-						 <select>
-							 <option>Select Size</option>
-							 <option>XL</option>
-							 <option>XXL</option>
-							 <option>Small</option>
-							 <option>Large</option>
-						 </select>
-						 <input type="number" value="1">
-						 <button>Add to Cart</button>
-						 <h4>Product Details</h4>
-						 <p>${ allproduct[id].ProductDetails}</p>
-					 </div>
-				 </div>
-			 </div>
-			 <!-- ======================end product====================== -->
-			 <!-- ======================start other products====================== -->
-			 <div class="products our-product">
-				 <div class="container">
-					 <div class="header">
-						 <h3>Related Products</h3>
-					 </div>
-					 <div class="content">
-					 </div>
-				 </div>
-			 </div>
-			 <!-- ======================end other products====================== -->
-	 `; 
-	 activeImage = document.querySelector('.product .images .active')
-	featureProduct = document.querySelector('.our-product .container .content')
-}
-//  change color of product
- 
-
-function changeColor(newImage){
-	activeImage.src = newImage
+// Open product details
+function openProduct(id) {
+    const product = allproduct[id];
+    activeProductDetails.innerHTML = `
+        <div class="product">
+            <div class="container">
+                <div class="images">
+                    <img src="${product.img}" class="active">
+                    <div class="color-product">
+                        ${product.productColors.map((color, index) => `
+                            <img src="${color}" onclick="changeColor(this.src)" id="color-${index + 1}">
+                        `).join('')}
+                    </div>
+                </div>
+                <div class="info">
+                    <span>${product.category}</span>
+                    <h3>${product.name}</h3>
+                    <span class="price">${product.price}</span>
+                    <select>
+                        <option>Select Size</option>
+                        <option>XL</option>
+                        <option>XXL</option>
+                        <option>Small</option>
+                        <option>Large</option>
+                    </select>
+                    <input type="number" value="1">
+                    <button>Add to Cart</button>
+                    <h4>Product Details</h4>
+                    <p>${product.ProductDetails}</p>
+                </div>
+            </div>
+        </div>
+        <div class="products our-product">
+            <div class="container">
+                <div class="header">
+                    <h3>Related Products</h3>
+                </div>
+                <div class="content"></div>
+            </div>
+        </div>
+    `;
+    activeImage = document.querySelector('.product .images .active');
+    featureProduct = document.querySelector('.our-product .container .content');
 }
 
-// product in webpage
-function addproducts(fileName, locationOfProducts){
-	fetch('product.json')
-			.then(response => response.json())
-			.then(data => {
-				const products = document.querySelector(locationOfProducts);
-				allproduct =data;
-
-				
-				data.forEach( product => {
-					if(product.section === fileName){
-					products.innerHTML += `
-						<div class="box">
-							<div class="image">
-								<img src="${product.img}">
-							</div>
-							<div class="stars">
-								<i class="fas fa-star"></i>
-								<i class="fas fa-star"></i>
-								<i class="fas fa-star"></i>
-								<i class="fas fa-star"></i>
-								<i class="fas fa-star"></i>
-							</div>
-							<p onclick="openProduct(${product.id})" product-id="${product.id}">${product.name}</p>
-							<span>$${product.price}</span>
-							<button>Add Cart</button>
-						</div>`
-				}else{
-				console.log("erro")	
-			}})
-			})
+// Change product color
+function changeColor(newImage) {
+    activeImage.src = newImage;
 }
-addproducts('shop', '.shop .content');
-addproducts('Featured', '.featured .content');
-addproducts('Dresses', '.dresses-jumpsuits .content');
-addproducts('Shoes', '.shoes .content');
-addproducts('Featured', '.our-product .container .content');
 
+// Add products to webpage
+function addProducts(fileName, locationOfProducts) {
+    fetch('product.json')
+        .then(response => response.json())
+        .then(data => {
+            allproduct = data;
+            const products = document.querySelector(locationOfProducts);
+            const filteredProducts = data.filter(product => product.section === fileName);
 
+            if (filteredProducts.length) {
+                filteredProducts.forEach(product => {
+                    products.innerHTML += `
+                        <div class="box">
+                            <div class="image">
+                                <img src="${product.img}">
+                            </div>
+                            <div class="stars">
+                                ${'<i class="fas fa-star"></i>'.repeat(5)}
+                            </div>
+                            <p onclick="openProduct(${product.id})" product-id="${product.id}">${product.name}</p>
+                            <span>$${product.price}</span>
+                            <button>Add to Cart</button>
+                        </div>
+                    `;
+                });
+            } else {
+                console.log("No products found in this section.");
+            }
+        })
+        .catch(error => console.error('Error fetching product data:', error));
+}
 
-
-
-
-
-
-
-
-
-
-
-
+// Load products into sections
+['shop', 'Featured', 'Dresses', 'Shoes'].forEach(section => {
+    addProducts(section, `.${section.toLowerCase()} .content`);
+});
