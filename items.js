4110410b7d66549848a232578e83fd46df86503b
egyption-pharhon <egyptionpharhon@gmail.com>
2024-09-19 05:34:13 +0200

    Update items.js

--- items.js
+++ items.js
@@ -104,8 +104,36 @@ addproducts('shop', '.shop .content');
 addproducts('Featured', '.featured .content');
 addproducts('Dresses', '.dresses-jumpsuits .content');
 addproducts('Shoes', '.shoes .content');
-addproducts('Featured', featureProduct);
 
+	fetch('product.json')
+			.then(response => response.json())
+			.then(data => {
+ 				featureProduct 
+				allproduct =data;
+
+				
+				data.forEach( product => {
+					if(product.section === "Featured"){
+					featureProduct.innerHTML += `
+						<div class="box">
+							<div class="image">
+								<img src="${product.img}">
+							</div>
+							<div class="stars">
+								<i class="fas fa-star"></i>
+								<i class="fas fa-star"></i>
+								<i class="fas fa-star"></i>
+								<i class="fas fa-star"></i>
+								<i class="fas fa-star"></i>
+							</div>
+							<p onclick="openProduct(${product.id})" product-id="${product.id}">${product.name}</p>
+							<span>$${product.price}</span>
+							<button>Add Cart</button>
+						</div>`
+				}else{
+				console.log("erro")	
+			}})
+			})
 
 
 
