5d39eb30c6aa517dd483ed426c42c679d3e7eca6
egyption-pharhon <egyptionpharhon@gmail.com>
2024-09-20 01:29:52 +0200

    Update main.js

--- main.js
+++ main.js
@@ -5,14 +5,14 @@ function openNavList(){
 	navlist.classList.toggle("show");
 }
 
-// open details of product
 let allproduct =[]
 
 let activeImage;
 let featureProduct;
-let totalofall = 0;
+let totalOfAllProductInCart = 0;
 let shipping = 35;
 let activeProductDetails = document.querySelector('.contentOfPage')
+
 // product in webpage
 function addproducts(fileName, locationOfProducts){
 	fetch('product.json')
@@ -41,59 +41,63 @@ function addproducts(fileName, locationOfProducts){
 					}
 				})
 			})
+            .catch(console.log('error'));
 }
 addproducts('shop', '.shop .content');
 addproducts('Featured', '.featured .content');
 addproducts('Dresses', '.dresses-jumpsuits .content');
 addproducts('Shoes', '.shoes .content');
 
-	function openProduct(id) {
-		activeProductDetails.innerHTML =`
-			 <!-- ======================start product====================== -->
-			 <div class="product">
-				 <div class="container">
-					 <div class="images">
-						 <img src="${ allproduct[id].img}" class="active">
-						 <div class="color-product">
-							 <img src="${ allproduct[id].productColors[0]}" onclick="changeColor(this.src)" id ="one">
-							 <img src="${ allproduct[id].productColors[1]}" onclick="changeColor(this.src)" id ="two">
-							 <img src="${ allproduct[id].productColors[2]}" onclick="changeColor(this.src)" id ="three">
-							 <img src="${ allproduct[id].productColors[3]}" onclick="changeColor(this.src)" id ="four">
-						 </div>
-					 </div>
-					 <div class="info">
-						 <span>${ allproduct[id].category}</span>
-						 <h3>${ allproduct[id].name}</h3>
-						 <span class="price">${allproduct[id].price}</span>
-						 <select>
-							 <option>Select Size</option>
-							 <option>XL</option>
-							 <option>XXL</option>
-							 <option>Small</option>
-							 <option>Large</option>
-						 </select>
-						 <input type="number" value="1">
-						 <button id="addElement">Add to Cart</button>
-						 <h4>Product Details</h4>
-						 <p>${ allproduct[id].ProductDetails}</p>
-					 </div>
-				 </div>
-			 </div>
-			 <!-- ======================end product====================== -->
-			 <!-- ======================start other products====================== -->
-			 <div class="products our-product" id="product">
-				 <div class="container">
-					 <div class="header">
-						 <h3>Related Products</h3>
-					 </div>
-					 <div class="content">
-                    
-					 </div>
-				 </div>
-			 </div>
-			 <!-- ======================end other products====================== -->
-	 `; 
-	 activeImage = document.querySelector('.product .images .active')
+// open details of product
+function openProduct(id) {
+    activeProductDetails.innerHTML =`
+            <!-- ======================start product====================== -->
+            <div class="product">
+                <div class="container">
+                    <div class="images">
+                        <img src="${ allproduct[id].img}" class="active">
+                        <div class="color-product">
+                            <img src="${ allproduct[id].productColors[0]}" onclick="changeColor(this.src)" id ="one">
+                            <img src="${ allproduct[id].productColors[1]}" onclick="changeColor(this.src)" id ="two">
+                            <img src="${ allproduct[id].productColors[2]}" onclick="changeColor(this.src)" id ="three">
+                            <img src="${ allproduct[id].productColors[3]}" onclick="changeColor(this.src)" id ="four">
+                        </div>
+                    </div>
+                    <div class="info">
+                        <span>${ allproduct[id].category}</span>
+                        <h3>${ allproduct[id].name}</h3>
+                        <span class="price">${allproduct[id].price}</span>
+                        <select>
+                            <option>Select Size</option>
+                            <option>XL</option>
+                            <option>XXL</option>
+                            <option>Small</option>
+                            <option>Large</option>
+                        </select>
+                        <input type="number" value="1">
+                        <button id="addElement">Add to Cart</button>
+                        <h4>Product Details</h4>
+                        <p>${ allproduct[id].ProductDetails}</p>
+                    </div>
+                </div>
+            </div>
+            <!-- ======================end product====================== -->
+            <!-- ======================start other products====================== -->
+            <div class="products our-product" id="product">
+                <div class="container">
+                    <div class="header">
+                        <h3>Related Products</h3>
+                    </div>
+                    <div class="content">
+                
+                    </div>
+                </div>
+            </div>
+            <!-- ======================end other products====================== -->
+	`; 
+
+	activeImage = document.querySelector('.product .images .active')
+
 	setTimeout(() => {
 		featureProduct = document.querySelector('#product .container .content')
 		fetch('product.json')
@@ -239,12 +243,12 @@ function totalOfAllProducts() {
     inputs.forEach(input => {
         const id = parseInt(input.dataset.id);
         const priceOfProduct = allproduct[id].price;
-        totalofall += input.value * priceOfProduct;
+        totalOfAllProductInCart += input.value * priceOfProduct;
     });
 
-    document.querySelector('.cart-total .subtotal').innerHTML =`${totalofall}`;
+    document.querySelector('.cart-total .subtotal').innerHTML =`${totalOfAllProductInCart}`;
     document.querySelector('.cart-total .shipping').innerHTML =`${shipping}`;
-    document.querySelector('.cart-total .total').innerHTML =`${(totalofall + shipping).toFixed(2)}`;
+    document.querySelector('.cart-total .total').innerHTML =`${totalOfAllProductInCart + shipping).toFixed(2)}`;
 }
 
 window.onload = loadCart;
