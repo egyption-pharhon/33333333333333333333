04371a4cb0b5d55d48070c750ca4ab78422370b2
egyption-pharhon <egyptionpharhon@gmail.com>
2024-09-19 02:53:17 +0200

    Update main.js

--- main.js
+++ main.js
@@ -12,42 +12,7 @@ let allProduct = [];
 
 // تغيير لون المنتج
 
-function openProduct(id) {
-   activeProductDetails.innerHTML =`
-		<!-- ======================start product====================== -->
-		<div class="product">
-			<div class="container">
-				<div class="images">
-					<img src="${ allproduct[id].img}" class="active">
-					<div class="color-product">
-						<img src="${ allproduct[id].productColors[0]}" onclick="changeColor(${ allproduct[id].productColors[0]})" id ="one">
-						<img src="${ allproduct[id].productColors[1]}" onclick="changeColor(${ allproduct[id].productColors[1]})" id ="two">
-						<img src="${ allproduct[id].productColors[2]}" onclick="changeColor(${ allproduct[id].productColors[2]})" id ="three">
-						<img src="${ allproduct[id].productColors[3]}" onclick="changeColor(${ allproduct[id].productColors[3]})" id ="four">
-					</div>
-				</div>
-				<div class="info">
-					<span>${ allproduct[id].category}</span>
-					<h3>${ allproduct[id].name}</h3>
-					<span class="price">${allproduct[id].price}</span>
-					<select>
-						<option>Select Size</option>
-						<option>XL</option>
-						<option>XXL</option>
-						<option>Small</option>
-						<option>Large</option>
-					</select>
-					<input type="number" value="1">
-					<button>Add to Cart</button>
-					<h4>Product Details</h4>
-					<p>${ allproduct[id].ProductDetails}</p>
-				</div>
-			</div>
-		</div>
-		<!-- ======================end product====================== -->
-		
-`;
-}
+
 
 // عناصر الألوان
 let activeOne = document.querySelector('#farag .container .images .color-product #one');
