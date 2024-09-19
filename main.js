757b87afce88f4c4bcad6cf789a6907868e092d8
egyption-pharhon <egyptionpharhon@gmail.com>
2024-09-19 03:22:02 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,80 @@
-// فتح قائمة العناصر عند حجم الشاشة 767 بكسل
-function openNavList() {
-    const navlist = document.querySelector('nav');
-    navlist.classList.toggle("show");
+// open list items @ media 767px
+
+function openNavList(){
+	let navlist = document.querySelector('nav');
+	navlist.classList.toggle("show");
 }
 
-// مصفوفة لتخزين جميع المنتجات
-let allProduct = [];
+// open details of product
+let allproduct =[]
 
-// فتح تفاصيل المنتج
+let activeImage;
+
+function openProduct(id){
+	function openProduct(id) {
+		activeProductDetails.innerHTML =`
+			 <!-- ======================start product====================== -->
+			 <div class="product">
+				 <div class="container">
+					 <div class="images">
+						 <img src="${ allproduct[id].img}" class="active">
+						 <div class="color-product">
+							 <img src="${ allproduct[id].productColors[0]}" onclick="changeColor(this.src)" id ="one">
+							 <img src="${ allproduct[id].productColors[1]}" onclick="changeColor(this.src)" id ="two">
+							 <img src="${ allproduct[id].productColors[2]}" onclick="changeColor(this.src)" id ="three">
+							 <img src="${ allproduct[id].productColors[3]}" onclick="changeColor(this.src)" id ="four">
+						 </div>
+					 </div>
+					 <div class="info">
+						 <span>${ allproduct[id].category}</span>
+						 <h3>${ allproduct[id].name}</h3>
+						 <span class="price">${allproduct[id].price}</span>
+						 <select>
+							 <option>Select Size</option>
+							 <option>XL</option>
+							 <option>XXL</option>
+							 <option>Small</option>
+							 <option>Large</option>
+						 </select>
+						 <input type="number" value="1">
+						 <button>Add to Cart</button>
+						 <h4>Product Details</h4>
+						 <p>${ allproduct[id].ProductDetails}</p>
+					 </div>
+				 </div>
+			 </div>
+			 <!-- ======================end product====================== -->
+			 <!-- ======================start other products====================== -->
+			 <div class="products our-product">
+				 <div class="container">
+					 <div class="header">
+						 <h3>Related Products</h3>
+					 </div>
+					 <div class="content">
+					 </div>
+				 </div>
+			 </div>
+			 <!-- ======================end other products====================== -->
+	 `; 
+	 activeImage = document.querySelector('.product .images .active')
+}
+//  change color of product
+ 
+
+function changeColor(newImage){
+	activeImage.src = newImage
+}
 
 
-// تغيير لون المنتج
 
 
 
-// عناصر الألوان
-let activeOne = document.querySelector('#farag .container .images .color-product #one');
-let activeTwo = document.querySelector('#farag .container .images .color-product #two');
-let activeThree = document.querySelector('#farag .container .images .color-product #three');
-let activeFour = document.querySelector('#farag .container .images .color-product #four');
-let activeDetails = document.querySelector('#farag .container .images .info p');
-let activeProductDetails = document.querySelector('.contentOfPage');
+
+
+
+
+
+
+
 
 
