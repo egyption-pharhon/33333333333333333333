76bc95a3d457176b04748b4856e9da0a3f57bea3
egyption-pharhon <egyptionpharhon@gmail.com>
2024-09-19 07:37:36 +0200

    Update main.js

--- main.js
+++ main.js
@@ -98,33 +98,43 @@ function changeColor(newImage){
 
 
 // add product to cart
-function addToCart(id){
-	fetch('cart.html')
-				.then(response => response.text())
-				.then(data => {
-					const elementInCart =document.querySelector('.cart table tbody')
-					console.log(elementInCart)
-						elementInCart.innerHTML +=`
-      <tr>
-								<td>
-									<i class="fa-regular fa-trash-can"></i>
-								</td>
-								<td>
-									<img src="img/shop/1.jpg">
-								</td>
-								<td>
-									<h4>Men's Fashion T-Shirt</h4>
-								</td>
-								<td>
-									<h4>$92.00</h4>
-								</td>
-								<td>
-									<input type="number" value="1">
-								</td>
-								<td>
-									<h4>$92.00</h4>
-								</td>
-							</tr>`;
-					})
-		
-}
+document.addEventListener('DOMContentLoaded', function() {
+    function addToCart(id) {
+        fetch('cart.html')
+            .then(response => response.text())
+            .then(data => {
+                // التأكد من وجود عنصر الـ cart في الصفحة الحالية
+                const elementInCart = document.querySelector('.cart table tbody');
+                console.log(elementInCart);
+
+                // تحقق إذا كان العنصر موجودًا
+                if (elementInCart) {
+                    // إضافة الصف إلى الجدول
+                    elementInCart.innerHTML += `
+                        <tr>
+                            <td>
+                                <i class="fa-regular fa-trash-can"></i>
+                            </td>
+                            <td>
+                                <img src="img/shop/1.jpg">
+                            </td>
+                            <td>
+                                <h4>Men's Fashion T-Shirt</h4>
+                            </td>
+                            <td>
+                                <h4>$92.00</h4>
+                            </td>
+                            <td>
+                                <input type="number" value="1">
+                            </td>
+                            <td>
+                                <h4>$92.00</h4>
+                            </td>
+                        </tr>`;
+                } else {
+                    console.error('العنصر .cart table tbody غير موجود في الصفحة.');
+                }
+            })
+            .catch(error => console.error('خطأ:', error));
+    }
+});
